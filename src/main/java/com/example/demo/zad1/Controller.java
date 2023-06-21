af2245106aae611b85d5efeb5d021eef89beb68a
magiczny <magicznyjasio@gmail.com>
2023-06-21 17:07:22 +0200

    WithoutTimezone

--- src/main/java/com/example/demo/zad1/Controller.java
+++ src/main/java/com/example/demo/zad1/Controller.java
@@ -16,7 +16,11 @@ public class Controller {
     @GetMapping("/info")
     public static String printSite(HttpServletRequest request) throws SocketException {
 
-
+      try (java.util.Scanner s = new java.util.Scanner(new java.net.URL("https://api.ip2location.io/?key=F662197B9997A4BE246F0111A25FFFA1&ip=2a01:118f:642:1400:ece3:240:e605:9fd&format=json&lang=en").openStream(), "UTF-8").useDelimiter("\\A")) {
+           System.out.println(s.next());
+      } catch (java.io.IOException e) {
+         e.printStackTrace();
+       }
 
         return "ip: " + request.getRemoteAddr();
     }
